d7dc4fd47c15156d5322fb2838c0e5215da26a18
MrSwitch <sonofdod@gmail.com>
2011-09-01 11:55:35 -0700

    Clean FashionTweek Code

--- sites/FashionTweek/script.js
+++ sites/FashionTweek/script.js
@@ -23,14 +23,10 @@ $(function(){
 		navigator.geolocation.getCurrentPosition(function(r){
 			// save the users position
 			coords = r.coords;
-			console.log(coords);
-		}, error);
+			log(coords);
+		}, log);
 	} else {
-		error('not supported');
-	}
-
-	function error(e){
-		try{console.log(e);}catch(e){}
+		log('!navigator.geolocation');
 	}
 	
 	var FashionCapitals = {
@@ -56,7 +52,7 @@ $(function(){
 				for(var i=0;i<r.results.length;i++){
 					var m = r.results[i].text.match(/http\:\/\/(twitpic.com|t\.co)\/\w+/);
 
-					if(m&&m[0]){
+					if(m&&m[0]&&unique(m[0])){
 						var $img = $("<figure><div class='img'></div><figcaption>"+r.results[i].text+"</figcaption></figure>").appendTo($div).find(".img");
 						$img.load("http://sandbox.knarly.com/proxy.php?path="+ m[0] + " #photo-display");
 					}
@@ -68,15 +64,9 @@ $(function(){
 
 	// Image unique?
 	var unique = (function(){
-		var archive = [];
+		var a = [];
 		return function(s){
-			for(var i=0;i<archive.length;i++){
-				if(archive[i]===s){
-					return false;
-				}
-			}
-			archive.push(s);
-			return true;
+			return ( a.indexOf(s)>-1 ? true : !a.push(s) );
 		};
 	})();
 	
@@ -119,7 +109,7 @@ $(function(){
 				$placeholderTop = $("<div></div>").css({height:0}).prependTo(".container").animate({height:height+"px"},"slow"),
 				$placeholderSection = $("<div></div>").insertAfter($section).css({height:height+"px"}).animate({height:0},"slow");
 			
-			console.log($section);
+			log($section);
 			
 			$section.css({position:'absolute', top: $section.position().top+"px", zIndex : 100000, width: width+"px" }).animate({top:$("section:first-of-type").position().top+"px"},"slow", function(){
 	
@@ -136,4 +126,21 @@ $(function(){
 });
 	
 
+function distance(latlon1, latlon2){
+
+	var lat1 = latlon1.split('.')[0],
+		lon1 = latlon1.split('.')[1],
+		lat2 = latlon2.split('.')[0],
+		lon2 = latlon2.split('.')[1];
 	
+	var R = 6371; // km
+	var dLat = (lat2-lat1).toRad();
+	var dLon = (lon2-lon1).toRad();
+	var lat1 = lat1.toRad();
+	var lat2 = lat2.toRad();
+	
+	var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+	        Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2); 
+	var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+	var d = R * c;
+}
