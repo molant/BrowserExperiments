a1a38a055319199b7ec89c78ecb553ff59a6c24f
MrSwitch <sonofdod@gmail.com>
2011-09-08 10:24:05 -0700

    Fix domain in TweetYvent

--- sites/TweetYvent/_config.php
+++ sites/TweetYvent/_config.php
@@ -1,6 +1,6 @@
 <?php 
 
-$NODE_DOMAIN = "peppy.browserexperiments.com"; //client access to the node server
+$NODE_DOMAIN = "browserexperiments.com"; //client access to the node server
 
 $MONGO_HOST = "localhost"; //web server access to the mongo server and the MongoDB database
 $GA_ANALYTICS = false; //Turn on/off Google Analytics - also add your analytics id to _foot.php
